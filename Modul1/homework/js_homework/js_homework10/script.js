bfde81981985f8119196939e727c2d36852ce5b9
Roman Karasevych <karasevich.roman@gmail.com>
2022-06-21 15:04:58 +0300

    complete js_homework10

--- Modul1/homework/js_homework/js_homework10/script.js
+++ Modul1/homework/js_homework/js_homework10/script.js
@@ -9,3 +9,31 @@
  Потрібно передбачити, що текст на вкладках може змінюватись, і що вкладки можуть додаватися та видалятися.
  При цьому потрібно, щоб функція, написана в джаваскрипті, через такі правки не переставала працювати.
  **/
+
+const tabItem = document.querySelectorAll(".tabs-title");
+const tabs = document.querySelector('.tabs');
+const tabContainer = document.querySelectorAll('.tab-content');
+
+
+const toggleCssClass = (nodeArr, cssClass, activeElement) => {
+    nodeArr.forEach((nodeItem) => {
+        if(nodeItem.classList.contains(cssClass)) {
+            nodeItem.classList.remove(cssClass)
+        }
+    })
+    activeElement.classList.add(cssClass);
+}
+
+const tabsFn = (event) => {
+    const id = event.target.getAttribute('data-tab');
+    const activeElement = document.getElementById(id)
+    toggleCssClass(tabItem,  'active', event.target)
+    toggleCssClass(tabContainer, 'active', activeElement)
+}
+
+tabs.addEventListener('click', (event)=>{
+    if(event.target.nodeName === 'LI') {
+        tabsFn(event)
+    }
+
+})
